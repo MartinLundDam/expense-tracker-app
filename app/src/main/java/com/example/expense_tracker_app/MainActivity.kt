c270caae0b90f541cdff44d41e0873e6570348fd
MartinLundDam <martinlunddam@hotmail.dk>
2024-11-04 14:31:01 +0800

    Remove access in MainActivity.kt

--- app/src/main/java/com/example/expense_tracker_app/MainActivity.kt
+++ app/src/main/java/com/example/expense_tracker_app/MainActivity.kt
@@ -17,31 +17,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            ExpensetrackerappTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+    }
+}
+/*
+setContent {
+    ExpensetrackerappTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Greeting(
+                name = "Android",
+                modifier = Modifier.padding(innerPadding)
+            )
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ExpensetrackerappTheme {
-        Greeting("Android")
-    }
-}
+ */
